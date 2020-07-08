41573b54d504fa7c853250ab362ea5e458bdfa7a
Eddy Rufino <eddyjaair@gmail.com>
2020-07-07 20:56:00 -0500

    add routes, models, files controllers and migrations with pivot

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\User;
 
 class CreateUsersTable extends Migration
 {
@@ -19,6 +20,9 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('verified')->default(User::USUARIO_NO_VERIFICADO);
+            $table->string('verification_token')->nullable();
+            $table->string('admin')->default(User::USUARIO_REGULAR);
             $table->rememberToken();
             $table->timestamps();
         });
--- routes/api.php
+++ routes/api.php
@@ -3,17 +3,20 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+// Buyer
+Route::resource('buyers', 'Buyer\BuyerController')->only(['index', 'show']);
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Category
+Route::resource('categories', 'Category\CategoryController')->except(['create', 'edit']);
+
+// Products
+Route::resource('products', 'Product\ProductController')->only(['index', 'show']);
+
+// Transactions
+Route::resource('transactions', 'Transaction\TransactionController')->only(['index', 'show']);
+
+// Sellers
+Route::resource('sellers', 'Seller\SellerController')->only(['index', 'show']);
+
+// Users
+Route::resource('users', 'User\UserController')->except(['create', 'edit']);
