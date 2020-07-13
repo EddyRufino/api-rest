53c74e70aafe7564f4766e34cb8f8fdf62e5bc31
Eddy Rufino <eddyjaair@gmail.com>
2020-07-13 12:36:56 -0500

    add complex operations for Transactions

--- routes/api.php
+++ routes/api.php
@@ -4,19 +4,36 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 // Buyer
-Route::resource('buyers', 'Buyer\BuyerController')->only(['index', 'show']);
+Route::resource('buyers', 'Buyer\BuyerController')
+	->only(['index', 'show']);
+
+
 
 // Category
-Route::resource('categories', 'Category\CategoryController')->except(['create', 'edit']);
+Route::resource('categories', 'Category\CategoryController')
+	->except(['create', 'edit']);
+
+
 
 // Products
-Route::resource('products', 'Product\ProductController')->only(['index', 'show']);
+Route::resource('products', 'Product\ProductController')
+	->only(['index', 'show']);
+
+
 
 // Transactions
-Route::resource('transactions', 'Transaction\TransactionController')->only(['index', 'show']);
+Route::resource('transactions', 'Transaction\TransactionController')
+	->only(['index', 'show']);
+Route::resource('transactions.categories', 'Transaction\TransactionCategoryController')->only(['index']);
+Route::resource('transactions.sellers', 'Transaction\TransactionSellerController')->only(['index']);
+
 
 // Sellers
-Route::resource('sellers', 'Seller\SellerController')->only(['index', 'show']);
+Route::resource('sellers', 'Seller\SellerController')
+	->only(['index', 'show']);
+
+
 
 // Users
-Route::resource('users', 'User\UserController')->except(['create', 'edit']);
+Route::resource('users', 'User\UserController')
+	->except(['create', 'edit']);
