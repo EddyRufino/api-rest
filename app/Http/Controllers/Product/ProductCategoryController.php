427fa0a45f9b9e51e190e9f16b98cd7315ab9e5e
Eddy Rufino <eddyjaair@gmail.com>
2020-07-17 19:29:23 -0500

    Complex operations for Product

---
+++ app/Http/Controllers/Product/ProductCategoryController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers\Product;
+
+use App\Http\Controllers\ApiController;
+use Illuminate\Http\Request;
+use App\Category;
+use App\Product;
+
+class ProductCategoryController extends ApiController
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Product $product)
+    {
+        $categories = $product->categories;
+        return $this->showAll($categories);
+    }
+
+    public function update(Request $request, Product $product, Category $category)
+    {
+        //attach, sync, syncWithoutDetach
+        $product->categories()->syncWithoutDetaching([$category->id]);
+
+        return $this->showAll($product->categories);
+    }
+
+    public function destroy(Product $product)
+    {
+        if (!$product->categories()->find($category->id)) {
+            return $this->errorResponse('The specified category is not a category of this product', 404);
+        }
+
+        $product->categories()->detach($category->id);
+
+        return $this->showAll($product->categories);
+    }
+}
