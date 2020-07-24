69accdba18612615357045fbd3e3befe145247a1
Eddy Rufino <eddyjaair@gmail.com>
2020-07-24 10:20:22 -0500

    Custom cache

--- app/Traits/ApiResponse.php
+++ app/Traits/ApiResponse.php
@@ -3,6 +3,7 @@
 namespace App\Traits;
 
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Pagination\LengthAwarePaginator;
