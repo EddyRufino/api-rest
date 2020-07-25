9b31510e5b2cda2590746348d8ac64eefdec2c03
Eddy Rufino <eddyjaair@gmail.com>
2020-07-25 10:31:56 -0500

    add frontend [Login and delete register]

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -115,7 +115,11 @@ class Handler extends ExceptionHandler
 
     protected function unauthenticated($request, AuthenticationException $exception)
     {
-        return $this->errorResponse('Unauthenticated.', 401);        
+        if ($this->isFrontend($request)) {
+            return redirect()->guest('login');
+        }
+
+        return $this->errorResponse('Unauthenticated.', 401);  
     }
 
     /**
@@ -130,7 +134,20 @@ class Handler extends ExceptionHandler
     {
         $errors = $e->validator->errors()->getMessages();
 
+
+        if ($this->isFrontend($request)) {
+            return $request->ajax() ? response()->json($error, 422) : redirect()
+                ->back()
+                ->withInput($request->input())
+                ->withErrors($errors);
+        }
+
         return $this->errorResponse($errors, 422);
 
+    }
+
+    private function isFrontend($request)
+    {
+        return $request->acceptsHtml() && collect($request->route()->middleware())->contains('web');
     }
 }
